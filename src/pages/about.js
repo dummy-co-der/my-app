6f9b07128449c578c6f59c0ebcd992a48302de36
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-03-07 19:51:05 +0530

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,42 +9,45 @@ export default function About(){
 		    width: '400px',
 			height:'300px',
 			marginTop: '5%',
-			marginLeft: '100px',
+			marginLeft: '15px',
 			marginRight: '20px',
 			marginBottom: '10px',
 		 }}>
-		{/* <Box color="white" bgcolor="grey" p={15}> */}
+		
 		<h2 className="headings"> OUR GOAL </h2>
 		Our goal is to ensure that students love and explore with programming while getting lucrative jobs in the process.
-		{/* </Box> */}
+		
 		</div>
 
 		<div className="box1"style={{ height:'10px',
 		width: '400px',
 		height:'300px',
 		marginTop: '-20.90%',
-		marginLeft: '530px',
+		marginLeft: '470px',
 		marginRight: '150px',
 		marginBottom: '5px',
 		}}>
-		{/* <Box color="white" bgcolor="grey" p={15}> */}
-		<h2 className="headings"> OUR GOAL </h2>
-		Our goal is to ensure that students love and explore with programming while getting lucrative jobs in the process.
-		{/* </Box> */}
+		
+		<h2 className="headings"> Learn About Us </h2>
+		Codechef ADGITM chapter organises regular contests , hackathons , weekly and monthly meets , 
+		sessions with mentors and seniors , all to bring out the best in us and motivate us to aspire high in the future
+		
 		</div>
 
 		<div className="box1" style={{ height:'10px',
 		width: '400px',
 		height:'300px',
 		marginTop: '-20.5%',
-		marginLeft: '960px',
+		marginLeft: '925px',
 		marginRight: '150px',
 		marginBottom: '5px',
 		}}>
-		{/* <Box color="white" bgcolor="black" p={15}> */}
-			<h2 className="headings"> OUR GOAL </h2>
-		Our goal is to ensure that students love and explore with programming while getting lucrative jobs in the process.
-		{/* </Box> */}
+		
+			<h2 className="headings"> Why Should you Join Us? </h2>
+			You learn many things, like how to work with a team, how to manage the schedule along with your studies. 
+			Also, when you have an event, you have to manage finances, sponsorships, everything. 
+			So you learn how to talk to people, approach companies, how to talk to guests, how to deal with your 
+			college management [ laugh at the last one ]
 		</div>
 		</div>
 		
