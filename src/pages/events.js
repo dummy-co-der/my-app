2442ff2a156753e31b6eb0e3803e1791785b7b68
Anuj Maheshwari <maheshwarianuj1001@gmail.com>
2022-03-07 20:09:20 +0530

    update

--- src/pages/events.js
+++ src/pages/events.js
@@ -5,12 +5,16 @@ return (
 	<div className='Events'
 	style={{
 		display: 'flex',
+		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center',
 		height: '150vh'
 	}}
 	>
-	<h1>Welcome to CodeChef Events</h1>
+		
+	<h1>Welcome to CodeChef ADGITM</h1>
+	<h3> Our Departments where you can join us. </h3>
+	
 	</div>
 );
 };
