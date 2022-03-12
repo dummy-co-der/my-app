d4bf57221434ead374211dbc70f290b1e7995b70
Kartikey Gupta <kartikeyrajgupta007@gmail.com>
2022-03-12 18:31:22 +0530

    updated department page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,13 +11,14 @@ return (
 		alignItems: 'center',
 		height: '90vh',
 		backgroundImage: `url("./planet.jpg")`,
+		backgroundSize:'cover',
 		flexDirection:'column',
 		color:'white',
 	}}
 	>
 	<img src= "./mainlogo.png" alt="center" width="125" height="125"/>
 	 <h2>CodeChef ADGITM</h2>
-	 <h1>A taste of good life</h1>
+	 <h1>A Community propelling you to your future</h1>
 	 </div>
 	</>
 );
