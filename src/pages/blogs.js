17678c5178e5b8226c7127c68818e738fb7ae3f3
Kartikey Gupta <kartikeyrajgupta007@gmail.com>
2022-03-12 22:50:11 +0530

    added sponsor caraousel

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -8,13 +8,50 @@ return (
 		flexDirection: 'column',
      	justifyContent: 'column',
 		alignItems: 'center',
-		height: '100vh',
+		height: '75vh',
 	}}
 	>
 	<h3>CODECHEF BLOG</h3>
 	<h1> Our Latest News</h1>
 	<h1 alignItems='left'> October</h1>
-	<h1>Our Past Event Sponsers</h1>
+	<section>
+            <div class="sponsorsDiv">
+                <h2 class="sponsorsText">Our Past Event Sponsors</h2>
+                
+              </div>
+                <div class="sponsorsIconsDiv">
+                    
+                    <img class="sponsorsIconsImg" src="img\sponsors\axure.jpg" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\devfolio.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\gmc.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\filecoin.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\coding ninjas.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\coil.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\streamyard.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\tezos.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\clerky.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\taskade-circle-logo-full-black.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\polygon-logo.svg" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\hd-Logo.jpg" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\prepbytes.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\download.png" alt=""/>
+                   {/* /<!--repeat--> */}
+                    <img class="sponsorsIconsImg" src="img\sponsors\axure.jpg" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\devfolio.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\gmc.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\filecoin.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\coding ninjas.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\coil.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\streamyard.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\tezos.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\clerky.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\taskade-circle-logo-full-black.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\polygon-logo.svg" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\hd-Logo.jpg" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\prepbytes.png" alt=""/>
+                    <img class="sponsorsIconsImg" src="img\sponsors\download.png" alt=""/>
+                </div>
+            </section>
 	</div>
 
 
